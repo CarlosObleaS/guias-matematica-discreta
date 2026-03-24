96d72cabbde871137d072b73e27525f410908efa
CarlosObleaS <carlosobleas@hotmail.com>
2026-03-24 10:33:33 -0500

    Web funcionando sin errores

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,12 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://CarlosObleaS.github.io',
 	base: '/guias-matematica-discreta/',
 	integrations: [
 		starlight({
 			title: 'Guías de Matemática Discreta',
-			social: {
-				github: 'https://github.com/CarlosObleaS/guias-matematica-discreta',
-			},
 			sidebar: [
 				{
 					label: 'Guías Rápidas',
